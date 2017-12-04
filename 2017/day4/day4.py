7ac911ec87decad72663bb5f290e2e9e6467a7d7
Simon Walker <s.r.walker101@googlemail.com>
2017-12-04 20:32:01 +0000

    Finish day4

--- 2017/day4/day4.py
+++ 2017/day4/day4.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 
+def is_anagram(a, b):
+    ord_a = list(sorted(a))
+    ord_b = list(sorted(b))
+
+    return ord_a == ord_b
+
 def analyse_part1(text):
 
     valid_lines = 0
@@ -9,7 +15,35 @@ def analyse_part1(text):
             valid_lines += 1
     return valid_lines
 
+def analyse_part2(text):
+
+    valid_lines = 0
+
+    for line in text:
+        vals = set()
+        found_anagram = False
+        for word in line:
+            for other in vals:
+                if is_anagram(word, other):
+                    found_anagram = True
+                    break
+
+            if found_anagram:
+                break
+
+            vals.add(word)
+
+        if not found_anagram:
+            valid_lines += 1
+
+    return valid_lines
+
 
 with open('input') as infile:
     lines = (line.strip().split() for line in infile)
-    print(analyse_part1(lines))
+    print('part1:',analyse_part1(lines))
+
+
+with open('input') as infile:
+    lines = (line.strip().split() for line in infile)
+    print('part2:',analyse_part2(lines))
--- 2017/day4/test_day4.py
+++ 2017/day4/test_day4.py
@@ -1,5 +1,5 @@
 import pytest
-from day4 import analyse_part1
+from day4 import analyse_part1, analyse_part2, is_anagram
 
 
 @pytest.mark.parametrize('input,expected', [
@@ -9,3 +9,19 @@ from day4 import analyse_part1
 ])
 def test_part_one(input, expected):
     assert analyse_part1(input) == expected
+
+
+@pytest.mark.parametrize('input,expected', [
+    ([['abcde', 'fghij']], 1),
+    ([['abcde', 'xyz', 'ecdab']], 0),
+])
+def test_part_two(input, expected):
+    assert analyse_part2(input) == expected
+
+
+@pytest.mark.parametrize('input,expected', [
+    (('abc', 'cba'), True),
+    (('abc', 'cbd'), False),
+])
+def test_is_anagram(input, expected):
+    assert is_anagram(*input) == expected
