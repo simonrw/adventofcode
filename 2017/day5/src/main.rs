34d1f502c2b0e9bc0df7d66c24e0d3db9c353b54
Simon Walker <s.r.walker101@googlemail.com>
2017-12-05 23:11:26 +0000

    Finish day 5 part 1

---
+++ 2017/day5/src/main.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::Read;
+
+type Instruction = i32;
+
+
+fn main() {
+    let mut s = String::new();
+    let mut f = File::open("input.txt").expect("opening input file");
+    f.read_to_string(&mut s).expect("reading file contents");
+
+    println!("{}", final_instruction_part_1(&s));
+}
+
+fn parse_instructions(text: &str) -> Vec<Instruction> {
+    text.split_whitespace()
+        .map(|c| c.parse().expect("parsing integer"))
+        .collect()
+}
+
+fn final_instruction_part_1(text: &str) -> u32 {
+    let mut instructions = parse_instructions(text);
+    let mut pos: i32 = 0;
+    let mut steps = 0;
+
+    while let Some(i) = instructions.get_mut(pos as usize) {
+        pos += *i;
+        *i += 1;
+        steps += 1;
+    }
+
+    steps
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_instructions() {
+        let text = "0\n3\n0\n1\n3\n";
+        assert_eq!(parse_instructions(text), vec![0, 3, 0, 1, 3]);
+    }
+
+    #[test]
+    fn test_example_result() {
+        let text = "0\n3\n0\n1\n3\n";
+        assert_eq!(final_instruction_part_1(text), 4);
+    }
+}
