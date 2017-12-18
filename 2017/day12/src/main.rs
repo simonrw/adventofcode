9e00336be59e9dacc1067d173efd4f1ed02478ca
Simon Walker <s.r.walker101@googlemail.com>
2017-12-18 21:44:59 +0000

    Start (unfinished) day12

--- 2017/day12/src/main.rs
+++ 2017/day12/src/main.rs
@@ -2,8 +2,32 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn day12_part1(_text: &str) -> usize {
+fn day12_part1(text: &str) -> usize {
+    for line in text.lines() {
+        let parsed: Mapping = line.parse().unwrap();
+    }
+
     6
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Mapping {
+    src: usize,
+    dests: Vec<usize>,
+}
+
+impl std::str::FromStr for Mapping {
+    type Err = Box<std::error::Error>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split_whitespace().collect();
+        let src: usize = parts[0].parse().unwrap();
+        assert_eq!(parts[1], "<->");
+        let dests = parts.into_iter().skip(2).map(|text| text.replace(",", "").parse().unwrap()).collect();
+
+        Ok(Mapping {
+            src: src, dests: dests,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -21,5 +45,25 @@ mod tests {
 6 <-> 4, 5";
         let result = day12_part1(&example);
         assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_parse_simple_line() {
+        let line = "0 <-> 2";
+        let result = line.parse::<Mapping>().unwrap();
+        assert_eq!(result, Mapping {
+            src: 0,
+            dests: vec![2],
+        });
+    }
+
+    #[test]
+    fn test_parse_complex_line() {
+        let line = "4 <-> 2, 3, 6";
+        let result = line.parse::<Mapping>().unwrap();
+        assert_eq!(result, Mapping {
+            src: 4,
+            dests: vec![2, 3, 6],
+        });
     }
 }
