1033f43bfca1885403f57b0de7e527d332f35144
Simon Walker <s.r.walker101@googlemail.com>
2017-12-25 18:11:44 +0000

    Finished day 18 phase 1

--- 2017/day18/src/main.rs
+++ 2017/day18/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 type Register = char;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Value {
     R(Register),
-    V(i32),
+    V(i64),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -13,7 +15,7 @@ pub enum Instruction {
     Add(Register, Value),
     Mul(Register, Value),
     Mod(Register, Value),
-    Rcv(Register),
+    Rcv(Value),
     Jgz(Register, Value),
 }
 
@@ -28,7 +30,7 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
                     let r = words.next().unwrap().chars().next().unwrap();
                     let v = words.next().unwrap();
 
-                    let parsed = v.parse::<i32>();
+                    let parsed = v.parse::<i64>();
                     if let Ok(value) = parsed {
                         Instruction::Set(r, Value::V(value))
                     } else {
@@ -43,7 +45,7 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
                     let r = words.next().unwrap().chars().next().unwrap();
                     let v = words.next().unwrap();
 
-                    let parsed = v.parse::<i32>();
+                    let parsed = v.parse::<i64>();
                     if let Ok(value) = parsed {
                         Instruction::Add(r, Value::V(value))
                     } else {
@@ -58,7 +60,7 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
                     let r = words.next().unwrap().chars().next().unwrap();
                     let v = words.next().unwrap();
 
-                    let parsed = v.parse::<i32>();
+                    let parsed = v.parse::<i64>();
                     if let Ok(value) = parsed {
                         Instruction::Mul(r, Value::V(value))
                     } else {
@@ -73,7 +75,7 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
                     let r = words.next().unwrap().chars().next().unwrap();
                     let v = words.next().unwrap();
 
-                    let parsed = v.parse::<i32>();
+                    let parsed = v.parse::<i64>();
                     if let Ok(value) = parsed {
                         Instruction::Mod(r, Value::V(value))
                     } else {
@@ -88,7 +90,7 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
                     let r = words.next().unwrap().chars().next().unwrap();
                     let v = words.next().unwrap();
 
-                    let parsed = v.parse::<i32>();
+                    let parsed = v.parse::<i64>();
                     if let Ok(value) = parsed {
                         Instruction::Jgz(r, Value::V(value))
                     } else {
@@ -107,8 +109,14 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
             }
             "rcv" => {
                 let instruction = {
-                    let r = words.next().unwrap().chars().next().unwrap();
-                    Instruction::Rcv(r)
+                    let r = words.next().unwrap();
+                    let parsed = r.parse::<i64>();
+                    if let Ok(value) = parsed {
+                        Instruction::Rcv(Value::V(value))
+                    } else {
+                        let r = r.chars().next().unwrap();
+                        Instruction::Rcv(Value::R(r))
+                    }
                 };
                 result.push(instruction);
             }
@@ -118,16 +126,133 @@ pub fn parse_instructions(text: &str) -> Vec<Instruction> {
     result
 }
 
+#[derive(Default, Debug)]
+pub struct State {
+    pc: i64,
+    last_sound: i64,
+    registers: HashMap<char, i64>,
+}
+
+pub fn evaluate(instructions: &[Instruction]) -> i64 {
+    use Instruction::*;
+
+    let mut state = State::default();
+    loop {
+        let instruction = &instructions[state.pc as usize];
+        match instruction {
+            &Set(ref r, ref v) => {
+                match *v {
+                    Value::V(value) => {
+                        *state.registers.entry(*r).or_insert(0) = value;
+                    }
+                    Value::R(name) => {
+                        let value = state.registers[&name];
+                        *state.registers.entry(*r).or_insert(0) = value;
+                    }
+                }
+            }
+            &Add(ref r, ref v) => {
+                match *v {
+                    Value::V(value) => {
+                        *state.registers.entry(*r).or_insert(0) += value;
+                    }
+                    Value::R(name) => {
+                        let value = state.registers[&name];
+                        *state.registers.entry(*r).or_insert(0) += value;
+                    }
+                }
+            }
+            &Mul(ref r, ref v) => {
+                match *v {
+                    Value::V(value) => {
+                        *state.registers.entry(*r).or_insert(0) *= value;
+                    }
+                    Value::R(name) => {
+                        let value = state.registers[&name];
+                        *state.registers.entry(*r).or_insert(0) *= value;
+                    }
+                }
+            }
+            &Mod(ref r, ref v) => {
+                match *v {
+                    Value::V(value) => {
+                        *state.registers.entry(*r).or_insert(0) %= value;
+                    }
+                    Value::R(name) => {
+                        let value = state.registers[&name];
+                        *state.registers.entry(*r).or_insert(0) %= value;
+                    }
+                }
+            }
+            &Snd(ref r) => {
+                state.last_sound = state.registers[&r];
+            }
+            &Rcv(ref r) => {
+                match r {
+                    &Value::V(value) => {
+                        if value > 0 {
+                            return state.last_sound;
+                        }
+                    }
+                    &Value::R(name) => {
+                        let value = state.registers[&name];
+                        if value > 0 {
+                            return state.last_sound;
+                        }
+                    }
+                }
+            }
+            &Jgz(ref r, ref v) => {
+                let value = state.registers[&r];
+                if value > 0 {
+                    match v {
+                        &Value::V(vv) => {
+                            state.pc += vv;
+                            /* Do not increment the program counter */
+                            continue;
+                        }
+                        &Value::R(rr) => {
+                            let newvalue = state.registers[&rr];
+                            state.pc += newvalue;
+                        }
+                    }
+                }
+            }
+        }
+
+        state.pc += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let instructions = parse_instructions(input);
+    let value = evaluate(&instructions);
+    println!("VALUE: {}", value);
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    fn expected_instructions() -> Vec<Instruction> {
+        use Instruction::*;
+        vec![
+            Set('a', Value::V(1)),
+            Add('a', Value::V(2)),
+            Mul('a', Value::R('a')),
+            Mod('a', Value::V(5)),
+            Snd('a'),
+            Set('a', Value::V(0)),
+            Rcv(Value::R('a')),
+            Jgz('a', Value::V(-1)),
+            Set('a', Value::V(1)),
+            Jgz('a', Value::V(-2)),
+        ]
+    }
+
     #[test]
-    fn parse_example() {
+    fn test_parse_example() {
         let instructions = "set a 1
         add a 2
         mul a a
@@ -140,22 +265,13 @@ mod test {
         jgz a -2";
 
         let instructions = parse_instructions(instructions);
+        assert_eq!(instructions, expected_instructions());
+    }
 
-        use Instruction::*;
-        assert_eq!(
-            instructions,
-            vec![
-                Set('a', Value::V(1)),
-                Add('a', Value::V(2)),
-                Mul('a', Value::R('a')),
-                Mod('a', Value::V(5)),
-                Snd('a'),
-                Set('a', Value::V(0)),
-                Rcv('a'),
-                Jgz('a', Value::V(-1)),
-                Set('a', Value::V(1)),
-                Jgz('a', Value::V(-2)),
-            ]
-        );
+    #[test]
+    fn test_example_program() {
+        let instructions = expected_instructions();
+        let result = evaluate(&instructions);
+        assert_eq!(result, 4);
     }
 }
