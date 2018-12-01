652d1375ae2761700db7843bc8c65b21805eaf95
Simon Walker <s.r.walker101@googlemail.com>
2018-12-01 14:37:36 +0000

    Complete part 2

--- 2018/day1/main.go
+++ 2018/day1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -17,24 +18,51 @@ func main() {
 
 	var value int64
 	value = 0
-	for _, line := range lines {
-		runes := []rune(line)
-		intval, err := strconv.ParseInt(string(runes[1:]), 10, 64)
-		if err != nil {
-			panic(err)
-		}
 
-		if runes[0] == '+' {
-			value += intval
-		} else if runes[0] == '-' {
-			value -= intval
-		} else {
-			err := fmt.Errorf("invalid prefix: %c", runes[0])
+	var seenElements []int64
+	counter := 0
+
+	for {
+		for _, line := range lines {
+			runes := []rune(line)
+			intval, err := strconv.ParseInt(string(runes[1:]), 10, 64)
 			if err != nil {
 				panic(err)
 			}
+
+			if runes[0] == '+' {
+				value += intval
+			} else if runes[0] == '-' {
+				value -= intval
+			} else {
+				err := fmt.Errorf("invalid prefix: %c", runes[0])
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			if seenElementsContains(seenElements, value) {
+				fmt.Printf("First element seen twice: %d\n", value)
+				os.Exit(0)
+			}
+
+			seenElements = append(seenElements, value)
+		}
+
+		if counter == 0 {
+			fmt.Printf("Answer: %v\n", value)
+		}
+
+		counter++
+	}
+}
+
+func seenElementsContains(seenElements []int64, value int64) bool {
+	for _, testValue := range seenElements {
+		if testValue == value {
+			return true
 		}
 	}
 
-	fmt.Printf("Answer: %v\n", value)
+	return false
 }
