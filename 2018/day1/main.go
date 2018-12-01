20cd216a9997f12d6f0268910fb65bfa1f23d488
Simon Walker <s.r.walker101@googlemail.com>
2018-12-01 14:32:25 +0000

    Finish part 1

--- 2018/day1/main.go
+++ 2018/day1/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	dat, err := ioutil.ReadFile("./input")
+	if err != nil {
+		panic(err)
+	}
+	str := string(dat)
+	lines := strings.Split(str, "\n")
+
+	var value int64
+	value = 0
+	for _, line := range lines {
+		runes := []rune(line)
+		intval, err := strconv.ParseInt(string(runes[1:]), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		if runes[0] == '+' {
+			value += intval
+		} else if runes[0] == '-' {
+			value -= intval
+		} else {
+			err := fmt.Errorf("invalid prefix: %c", runes[0])
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	fmt.Printf("Answer: %v\n", value)
 }
