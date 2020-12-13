fb22dd9704ebc465195c1b438a383e6c942cc1ab
Simon Walker <s.r.walker101@googlemail.com>
2020-12-13 02:07:55 +0000

    Implements part two

--- 2020/day6/src/main.rs
+++ 2020/day6/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     let input = include_str!("../input");
     let part_one_total = group_sum(&input);
     println!("{}", part_one_total);
+
+    let part_two_total = all_group_sum(&input);
+    println!("{}", part_two_total);
 }
 
 fn group_sum(input: &str) -> usize {
@@ -27,12 +30,57 @@ fn group_sum(input: &str) -> usize {
     count
 }
 
+fn all_group_sum(input: &str) -> usize {
+    let mut count = 0;
+    let mut group_total: Vec<Vec<char>> = Vec::new();
+    for line in input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            count += finish_group(&group_total);
+            group_total.clear();
+        } else {
+            let group: Vec<_> = line.chars().collect();
+            group_total.push(group);
+        }
+    }
+
+    count += finish_group(&group_total);
+
+    count
+}
+
+fn finish_group(group_total: &Vec<Vec<char>>) -> usize {
+    let mut count = 0;
+    let mut values = HashSet::new();
+    for e in group_total {
+        for every in e {
+            values.insert(every);
+        }
+    }
+
+    for every in values {
+        let mut found = true;
+        for person in group_total {
+            if !person.contains(every) {
+                found = false;
+                break;
+            }
+        }
+
+        if found {
+            count += 1;
+        }
+    }
+
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn given_example() {
+    fn given_example_part_one() {
         let input = "abc
 
 a
@@ -51,5 +99,27 @@ b
 ";
 
         assert_eq!(group_sum(&input), 11);
+    }
+
+    #[test]
+    fn given_example_part_two() {
+        let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+";
+
+        assert_eq!(all_group_sum(&input), 6);
     }
 }
