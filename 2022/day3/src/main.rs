3a5261c5f7911aeb7ee2af41e2a392b155bd763d
Simon Walker <s.r.walker101@googlemail.com>
2023-02-28 10:46:10 +0000

    Add part2 solution

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -6,14 +6,18 @@ struct Backpack<'s> {
     right: &'s str,
 }
 
+fn score_letter(letter: char) -> u64 {
+    (if letter.is_uppercase() {
+        (letter as i32) - ('a' as i32) + 59
+    } else {
+        (letter as i32) - ('a' as i32) + 1
+    }) as _
+}
+
 impl<'s> Backpack<'s> {
     pub fn score(&self) -> u64 {
         let common_item = self.common_item();
-        (if common_item.is_uppercase() {
-            (common_item as i32) - ('a' as i32) + 59
-        } else {
-            (common_item as i32) - ('a' as i32) + 1
-        }) as _
+        score_letter(common_item)
     }
 
     fn common_item(&self) -> char {
@@ -37,16 +41,45 @@ fn parse_line(line: &str) -> Backpack<'_> {
     Backpack { left: l, right: r }
 }
 
-fn main() {
-    let input = include_str!("../input");
-    let lines = input.split_whitespace();
+fn compute_group_score(lines: &[&str]) -> u64 {
+    assert_eq!(lines.len(), 3);
+    let hashsets = lines.iter().map(|l| l.chars().collect::<HashSet<char>>());
+    let group_letter_set = hashsets
+        .reduce(|acc, e| acc.intersection(&e).cloned().collect())
+        .unwrap();
+    assert_eq!(group_letter_set.len(), 1);
+    score_letter(*group_letter_set.iter().next().unwrap())
+}
+
+fn part1<'a>(lines: impl Iterator<Item = &'a str>) {
     let mut sum = 0;
     for line in lines {
-        let backpack = parse_line(&line);
+        let backpack = parse_line(line);
         let score = backpack.score();
         sum += score;
     }
     println!("part 1: {sum}");
+}
+
+fn part2<'a>(input: &str) {
+    let grouped = input
+        .split_whitespace()
+        .step_by(3)
+        .zip(input.split_whitespace().skip(1).step_by(3))
+        .zip(input.split_whitespace().skip(2).step_by(3))
+        .map(|((a, b), c)| [a, b, c]);
+    let mut total = 0;
+    for group in grouped {
+        let score = compute_group_score(&group);
+        total += score;
+    }
+    println!("part 2: {total}");
+}
+
+fn main() {
+    let input = include_str!("../input");
+    part1(input.split_whitespace());
+    part2(&input);
 }
 
 #[cfg(test)]
@@ -76,5 +109,23 @@ CrZsJsPPZsGzwwsLwLmpwMDw
             let backpack = parse_line(&line);
             assert_eq!(backpack.score(), scores[i]);
         }
+    }
+
+    #[test]
+    fn group_score() {
+        let lines = [
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+        ];
+
+        assert_eq!(compute_group_score(&lines), 18);
+
+        let lines = [
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw",
+        ];
+        assert_eq!(compute_group_score(&lines), 52);
     }
 }
